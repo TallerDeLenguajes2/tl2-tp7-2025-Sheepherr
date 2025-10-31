760c6e053b8e79e95a23304afc3e9a8d23d20fcb
Luciano <ovejeromarcosluciano@gmail.com>
2025-10-31 00:24:53 -0300

    realizado el controll de productos

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,6 +1,7 @@
 namespace ProductoController;
 using Microsoft.AspNetCore.Mvc;
 using ProductosRepository;
+using Productos;
 
 
 [ApiController]
@@ -20,5 +21,46 @@ public IActionResult GetAll()
     {
         var productos = _productosRepository.GetAllProductos();
         return Ok(productos);
+    }
+    [HttpGet("producto/{id}")]
+    public IActionResult GetById(int id)
+    {
+        var producto = _productosRepository.GetbyIdProducto(id);
+        if (producto == null)
+            return NotFound($"No se encontró un producto con ID {id}");
+
+        return Ok(producto);
+    }
+
+    [HttpPost("producto")]
+    public IActionResult Create([FromBody] Productos nuevoProducto)
+    {
+        if (nuevoProducto == null)
+            return BadRequest("El producto no puede ser nulo.");
+
+        _productosRepository.InsertProducto(nuevoProducto);
+        return Created($"Producto/product/{nuevoProducto.idProducto}", nuevoProducto);
+    }
+
+    [HttpPut("producto/{id}")]
+    public IActionResult Update(int id, [FromBody] Productos productoActualizado)
+    {
+        var existente = _productosRepository.GetbyIdProducto(id);
+        if (existente == null)
+            return NotFound($"No existe producto con ID {id} para actualizar.");
+
+        _productosRepository.UpdateProducto(id, productoActualizado);
+        return Ok("Producto actualizado correctamente.");
+    }
+
+    [HttpDelete("producto/{id}")]
+    public IActionResult Delete(int id)
+    {
+        var existente = _productosRepository.GetbyIdProducto(id);
+        if (existente == null)
+            return NotFound($"No existe producto con ID {id} para eliminar.");
+
+        _productosRepository.DeleteProducto(id);
+        return Ok($"Producto con ID {id} eliminado correctamente.");
     }
 }
